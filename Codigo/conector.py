348db8ffd3bf5529b76710394ad0fc7945717a34
Yesica de matos <dematosyesicagabriela@gmail.com>
2022-09-11 21:22:44 -0300

    archivos eliminados

--- Codigo/conector.py
+++
@@ -1,2 +0,0 @@
-from sqlalchemy import create_engine
-enginok = create_engine('postgresql://postgres:ADMIN1234@localhost:5432/challenge_data_analyst')
--- Codigo/consulta1.sql
+++
@@ -1,7 +0,0 @@
-SELECT "Provincia" , sum ("Pantallas")as "Pantallas" ,sum("Butacas")as "Butacas" ,count("Espacio_Incaa") as "Espacio_Incaa" 
-FROM "CINES"
-NATURAL JOIN "Departamento"
-NATURAL JOIN "Localidad"
-NATURAL JOIN "Provincia"
-GROUP BY "Provincia"
-ORDER BY "Provincia"   ASC;
